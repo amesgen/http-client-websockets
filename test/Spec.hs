62f05e37eadfd26985363c44f679eb3fc7536e7d
amesgen <amesgen@amesgen.de>
2021-08-21 01:29:51 +0200

    Use local WS server
    TLS tests should probably be added back in the future

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,22 +3,32 @@
 
 module Main (main) where
 
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.Async (withAsync)
+import Control.Monad
 import Data.ByteString
-import Data.Foldable (for_)
 import Network.HTTP.Client.TLS (newTlsManager)
 import Network.HTTP.Client.WebSockets as HCWS
 import Network.URI.Static
 import qualified Network.WebSockets as WS
 import Test.Hspec
 
+-- TODO test TLS websockets
+
 main :: IO ()
 main = hspec $
   describe "WebSockets via http-client" $ do
     it "should connect to an echo server" $ do
       mgr <- newTlsManager
-      for_ [[uri|ws://echo.websocket.org|], [uri|wss://echo.websocket.org|]] $ \u ->
-        HCWS.runClient mgr u $ \conn -> do
+      withEchoServer $
+        HCWS.runClient mgr [uri|ws://localhost:12345|] $ \conn -> do
           let bs = "test" :: ByteString
           WS.sendTextData conn bs
           bs' <- WS.receiveData conn
           bs `shouldBe` bs'
+  where
+    withEchoServer ma = withAsync runEchoServer (const $ threadDelay 100000 *> ma)
+      where
+        runEchoServer = WS.runServer "localhost" 12345 $ \pendingConn -> do
+          conn <- WS.acceptRequest pendingConn
+          forever $ WS.receive conn >>= WS.send conn
